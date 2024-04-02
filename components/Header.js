a47a35838a2a80dee277dabee0a90bd6b796118d
Arrakeencode <bryanproinfo@gmail.com>
2024-04-02 19:12:13 +0200

    fix

--- components/Header.js
+++ components/Header.js
@@ -52,7 +52,7 @@ export default function Header() {
                                     className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
                                     href="#"
                                 >
-                                    S'inscrire
+                                    S inscrire
                                 </Link>
                                 <Link
                                     className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
