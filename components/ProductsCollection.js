44e823a3704e24cfb9730dbe1fa83eee486af55b
Arrakeencode <bryanproinfo@gmail.com>
2024-04-04 15:47:00 +0200

    fix panier add category

--- components/ProductsCollection.js
+++ components/ProductsCollection.js
@@ -1,16 +1,34 @@
 import Link from "next/link";
 import {CartContext} from "@/lib/context/CartContext";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 
 export default function ProductsCollection({allProducts}){
     const { addProduct } = useContext(CartContext)
-    function addItemToCart() {
-        addProduct(allProducts._id);
+
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleCategorySelect = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    // Filtrer les produits en fonction de la catégorie sélectionnée
+    const filteredProducts = selectedCategory
+        ? allProducts.filter(product => product.category === selectedCategory)
+        : allProducts;
+    function addItemToCart(productId) {
+        addProduct(productId);
     }
     return (
         <section>
+            <select value={selectedCategory} onChange={handleCategorySelect}>
+                <option value="">ko</option>
+                <option value="raquette">r</option>
+                <option value="balle">b</option>
+                <option value="sac">s</option>
+                <option value="cordage">c</option>
+            </select>
             <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                {allProducts?.length > 0 && allProducts.map((product) => (
+                {allProducts?.length > 0 && filteredProducts.map((product) => (
                     <div key={product.id}>
                         <li>
                             <div className="group block overflow-hidden">
@@ -42,7 +60,7 @@ export default function ProductsCollection({allProducts}){
                                 <div className="col-span-12 text-center w-full mt-3">
                                     <button
                                         className="disabled block rounded bg-secondary px-5 py-3 text-md text-text w-full transition hover:bg-purple-300"
-                                        data-dashlane-label="true" data-dashlane-rid="4f2810c601fdbcc7" data-form-type="" onClick={addItemToCart}>Add to
+                                        data-dashlane-label="true" data-dashlane-rid="4f2810c601fdbcc7" data-form-type="" onClick={() => addItemToCart(product._id)}>Add to
                                         cart
                                     </button>
                                 </div>
