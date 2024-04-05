2de9080d8abfd47f93423152aa60c924765c3a62
Arrakeencode <bryanproinfo@gmail.com>
2024-04-05 22:45:57 +0200

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,14 @@ import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react"
 import {CartContextProvider} from "@/lib/context/CartContext";
 import {Toaster} from "react-hot-toast";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+
   return <>
     <SessionProvider session={session}>
       <CartContextProvider>
@@ -16,9 +20,11 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
           <Header/>
           <Toaster position='top-center' />
           <Component {...pageProps}/>
+          <GoogleAnalytics gaId="G-T80XDD5K20" />
           <Footer/>
         </main>
       </CartContextProvider>
     </SessionProvider>
 </>
+
 }
